7513e2dbe052b8e0681d1b8b103b5b8b724945a7
Aleksandr <seoinetru@gmail.com>
2026-02-14 10:39:39 +0300

    Rebrand app as Li Monitor and add new professional icon

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "li_auto_monitor");
+    gtk_header_bar_set_title(header_bar, "Li Monitor");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "li_auto_monitor");
+    gtk_window_set_title(window, "Li Monitor");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
